e692f21c0adc08c8126426faafebccbecfbae29b
Pavel Drankov <titantins@gmail.com>
2024-06-27 03:21:51 +0300

    123

---
+++ README.md
@@ -0,0 +1,10 @@
+# AntiDOT
+
+List of classes and spells being tracked by AntiDOT:
+
+1. Ignite by fire mage
+2. Shadow Word: Pain by shadow priest
+3. Vampire Touch by shadow priest
+4. Devouring Plague by shadow priest
+5. Unstable Affliction by affliction warlock
+6. Immolate by destruction warlock
--- antidot.lua
+++ antidot.lua
@@ -3,7 +3,17 @@ function()
     -- 2. Shadow word pain
     -- 3. Vampire touch
     -- 4. Devouring plague
-
+    -- 5. Unstable affliction
+    -- 6. Corruption
+    -- 7. Shadow Embrace
+    -- 8. Agony
+    -- 9. Dread Touch
+    -- 10. Soul Rot
+    -- 11. Haunt
+    -- 12. Flame Shock
+    -- 13. Moonfire
+    -- 14. Sunfire
+    -- 15. Immolate
     function numToStr(num)
         local letter = {
             [0] = "",
@@ -31,12 +41,54 @@ function()
         return finalVal * multiplier
     end
 
+        
+    function mutliplierWa(trigger) 
+        local mutliplier = 1.0
+        if aura_env.states[trigger].show and aura_env.states[trigger].tooltip1 ~= nil  then
+            mutliplier = 1 + aura_env.states[trigger].tooltip1 / 100
+        end
+        return mutliplier
+    end
+    
+    local dbg = false 
     local ignite = simplehotValue(1)
     local shadowWordPain = simplehotValue(2)
     local vampiricTouch = simplehotValue(3)
     local devouringPlague = simplehotValue(4)
-    local total = ignite + shadowWordPain + vampiricTouch + devouringPlague
-    return numToStr(total)
+    local unstableAffliction = simplehotValue(5)
+    local corruption = simplehotValue(6)
+    local shadowEmbrace = mutliplierWa(7)
+    local agony = simplehotValue(8)
+    local dreadTouch = mutliplierWa(9)
+    local soulRot = simplehotValue(10)
+    local haunt = mutliplierWa(11)
+    local flameShock = simplehotValue(12)
+    local moonfire = simplehotValue(13)
+    local sunfire = simplehotValue(14)
+    local immolate = simplehotValue(15)
+
+    local total = immolate + moonfire + sunfire + flameShock +  ignite + shadowWordPain + vampiricTouch + devouringPlague + (unstableAffliction + corruption + agony + soulRot) * shadowEmbrace * dreadTouch  * haunt
+
+    if dbg then
+        DevTool:AddData(immolate, "immolate")
+        DevTool:AddData(sunfire, "sunfire")
+        DevTool:AddData(moonfire, "moonfire")
+        DevTool:AddData(flameShock, "flameShock")
+        DevTool:AddData(ignite, "ignite")
+        DevTool:AddData(shadowWordPain, "shadowWordPain")
+        DevTool:AddData(vampiricTouch, "vampiricTouch")
+        DevTool:AddData(devouringPlague, "devouringPlague")
+        DevTool:AddData(unstableAffliction, "unstableAffliction")
+        DevTool:AddData(corruption, "corruption")
+        DevTool:AddData(shadowEmbrace, "shadowEmbrace")
+        DevTool:AddData(agony, "agony")
+        DevTool:AddData(dreadTouch, "dreadTouch")
+        DevTool:AddData(soulRot, "soulRot")
+        DevTool:AddData(haunt, "haunt")
+        DevTool:AddData("total",total)
+    end
+
+    return numToStr(0 + total)
 end
 
 
